46d9f1f3de9c3e47191683e5779f4efba7d3bd8f
Alex Carufel <Carual42@gmail.com>
2022-10-13 21:37:07 -0500

    NewVisit component made

---
+++ src/components/NewVisit/NewVisit.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// This is one of our simplest components
+// It doesn't have local state,
+// It doesn't dispatch any redux actions or display any part of redux state
+// or even care what the redux state is'
+
+function Medication() {
+  return (
+    <div className="container">
+      <div>
+        <p>This page will have input fields for the user to enter new information</p>
+      </div>
+    </div>
+  );
+}
+
+export default Medication;
