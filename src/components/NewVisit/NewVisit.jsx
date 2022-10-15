6d7292fa3540cd0fcc75e36ca118cffcb1aa4157
Alex Carufel <Carual42@gmail.com>
2022-10-15 16:39:28 -0500

    feature done

--- src/components/NewVisit/NewVisit.jsx
+++ src/components/NewVisit/NewVisit.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextField } from '@mui/material';
+import { TextField, Button } from '@mui/material';
 // This is one of our simplest components
 // It doesn't have local state,
 // It doesn't dispatch any redux actions or display any part of redux state
@@ -12,23 +12,29 @@ function NewVisit() {
         <p>This page will have input fields for the user to enter new information</p>
         <br />
         <h2>Medications</h2>
-        <TextField id="filled-basic" label="Filled" variant="filled" /> 
-        <TextField id="filled-basic" label="Filled" variant="filled" />
-        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <TextField className="filled-basic" label="Phone" variant="filled" /> 
+        <TextField className="filled-basic" label="Medication and dose" variant="filled" />
+        <TextField className="filled-basic" label="Notes" variant="filled" />
+        <br />
+        <Button>Submit</Button>
         <br />
         <br />
         <br />
         <h2>Procedures</h2>
-        <TextField id="filled-basic" label="Filled" variant="filled" />
-        <TextField id="filled-basic" label="Filled" variant="filled" />
-        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <TextField className="filled-basic" label="Phone" variant="filled" />
+        <TextField className="filled-basic" label="Exam" variant="filled" />
+        <TextField className="filled-basic" label="Notes" variant="filled" />
+        <br />
+        <Button>Submit</Button>
         <br />
         <br />
         <br />
         <h2>Scans</h2>
-        <TextField id="filled-basic" label="Filled" variant="filled" />
-        <TextField id="filled-basic" label="Filled" variant="filled" />
-        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <TextField className="filled-basic" label="Phone" variant="filled" />
+        <TextField className="filled-basic" label="Scan" variant="filled" />
+        <TextField className="filled-basic" label="Notes" variant="filled" />
+        <br />
+        <Button>Submit</Button>
       </div>
     </div>
   );
