501423ec92a6785700de54e2f68dda01f41dcfdf
Alex Carufel <Carual42@gmail.com>
2022-10-15 16:38:21 -0500

    New visit Dom updated

--- src/components/NewVisit/NewVisit.jsx
+++ src/components/NewVisit/NewVisit.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { TextField } from '@mui/material';
 // This is one of our simplest components
 // It doesn't have local state,
 // It doesn't dispatch any redux actions or display any part of redux state
@@ -10,6 +10,25 @@ function NewVisit() {
     <div className="container">
       <div>
         <p>This page will have input fields for the user to enter new information</p>
+        <br />
+        <h2>Medications</h2>
+        <TextField id="filled-basic" label="Filled" variant="filled" /> 
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <br />
+        <br />
+        <br />
+        <h2>Procedures</h2>
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <br />
+        <br />
+        <br />
+        <h2>Scans</h2>
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <TextField id="filled-basic" label="Filled" variant="filled" />
       </div>
     </div>
   );
