159cd94a3114c53147bcf3a31b0003b58ed09c04
Alex Carufel <Carual42@gmail.com>
2022-10-16 17:11:21 -0500

    medication table displays properly

--- src/components/Medication/Medication.jsx
+++ src/components/Medication/Medication.jsx
@@ -9,18 +9,6 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-  }
-  
-  const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-  ];
-
 // This is one of our simplest components
 // It doesn't have local state,
 // It doesn't dispatch any redux actions or display any part of redux state
@@ -28,7 +16,7 @@ function createData(name, calories, fat, carbs, protein) {
 
 function Medication() {
 
-const [medlist, setMedList] = useState([]);
+const [medList, setMedList] = useState([]);
 
 useEffect(() => {
     fetchMeds();
@@ -56,18 +44,17 @@ const fetchMeds = () =>{
         </TableRow>
       </TableHead>
       <TableBody>
-        {rows.map((row) => (
+        {medList.map((med) => (
           <TableRow
-            key={row.name}
+            key={med.id}
             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
           >
             <TableCell component="th" scope="row">
-              {row.name}
+              {med.name}
             </TableCell>
-            <TableCell align="right">{row.calories}</TableCell>
-            <TableCell align="right">{row.fat}</TableCell>
-            <TableCell align="right">{row.carbs}</TableCell>
-            <TableCell align="right">{row.protein}</TableCell>
+            <TableCell align="right">{med.notes}</TableCell>
+            <TableCell align="right">{med.phone}</TableCell>
+            <TableCell align="right">{med.date}</TableCell>
           </TableRow>
         ))}
       </TableBody>
