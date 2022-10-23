870dabd3fcd017869732367ccfa5d4e5d6181906
Alex Carufel <Carual42@gmail.com>
2022-10-23 16:25:40 -0500

    inputs store to an object and are pushed to an array

--- src/components/NewVisit/NewVisit.jsx
+++ src/components/NewVisit/NewVisit.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import { TextField, Button } from '@mui/material';
+import {useEffect, useState} from 'react'
 // This is one of our simplest components
 // It doesn't have local state,
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is'
 
 function NewVisit() {
+  const [newMedVisit, setNewMedVisit] = useState([]);
+  const [medPhone, setMedPhone] = useState('');
+  const [med, setMed] = useState('');
+  const [medNote, setMedNote] = useState('');
+  const createMedVisit = () => {
+    let medVisit = {
+      phone: medPhone,
+      med: med,
+      note: medNote,
+    };
+    console.log('this is the object', medVisit);
+    newMedVisit.push(medVisit);
+    console.log('this is the array', newMedVisit);
+    setMedPhone('');
+    setMed('');
+    setMedNote('');
+  }
 
-  // const [value, setValue] = React.useState('Controlled');
+  let newProcedureVisit = [];
+  const [procedurePhone, setProcedurePhone] = useState('');
+  const [procedure, setProcedure] = useState('');
+  const [procedureNote, setProcedureNote] = useState('');
 
-  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setValue(event.target.value);
-  // };
+  let newExamVisit = [];
+  const [examPhone, setExamPhone] = useState('');
+  const [exam, setExam] = useState('');
+  const [examNote, setExamNote] = useState('');
+ 
 
   return (
     <div className="container">
@@ -19,29 +42,61 @@ function NewVisit() {
         <p>This page will have input fields for the user to enter new information</p>
         <br />
         <h2>Medications</h2>
-        <TextField className="filled-basic" multiline label="Phone" variant="filled" /> 
-        <TextField className="filled-basic" multiline label="Medication and dose" variant="filled" />
-        <TextField className="filled-basic" multiline label="Notes" variant="filled" />
+        <form>
+        <TextField  value={medPhone} 
+                    onChange={(event) => setMedPhone(event.target.value)}
+                    className="filled-basic" multiline 
+                    label="Phone" variant="filled" /> 
+        <TextField  value={med} 
+                    onChange={(event) => setMed(event.target.value)}
+                    className="filled-basic" multiline 
+                    label="Medication and dose" variant="filled" />
+        <TextField  value={medNote} 
+                    onChange={(event) => setMedNote(event.target.value)}
+                    className="filled-basic" multiline 
+                    label="Notes" variant="filled" />
         <br />
-        <Button>Submit</Button>
-        <br />
+        <Button onClick={() => createMedVisit()} >Submit</Button>
+        </form>
         <br />
         <br />
         <h2>Procedures</h2>
-        <TextField className="filled-basic" multiline label="Phone" variant="filled" />
-        <TextField className="filled-basic" multiline label="Exam" variant="filled" />
-        <TextField className="filled-basic" multiline label="Notes" variant="filled" />
+        <form>
+        <TextField  value={procedurePhone} 
+                    onChange={(event) => setProcedurePhone(event.target.value)}
+                    className="filled-basic" multiline 
+                    label="Phone" variant="filled" />
+        <TextField  value={procedure} 
+                    onChange={(event) => setProcedure(event.target.value)}
+                    className="filled-basic" multiline 
+                    label="Exam" variant="filled" />
+        <TextField  value={procedureNote} 
+                    onChange={(event) => setProcedureNote(event.target.value)}
+                    className="filled-basic" multiline
+                    label="Notes" variant="filled" />
         <br />
         <Button>Submit</Button>
+        </form>
         <br />
         <br />
         <br />
         <h2>Scans</h2>
-        <TextField className="filled-basic" multiline label="Phone" variant="filled" />
-        <TextField className="filled-basic" multiline label="Scan" variant="filled" />
-        <TextField className="filled-basic" multiline label="Notes" variant="filled" />
+        <form>
+        <TextField  value={examPhone} 
+                    onChange={(event) => setExamPhone(event.target.value)}
+                    className="filled-basic" multiline 
+                    label="Phone" variant="filled" />
+        <TextField  value={exam} 
+                    onChange={(event) => setExam(event.target.value)}
+                    className="filled-basic" multiline 
+                    label="Scan" variant="filled" />
+        <TextField value={examNote} 
+                    onChange={(event) => setExamNote(event.target.value)}
+                    className="filled-basic" multiline 
+                    label="Notes" variant="filled" />
         <br />
         <Button>Submit</Button>
+        </form>
       </div>
     </div>
   );
