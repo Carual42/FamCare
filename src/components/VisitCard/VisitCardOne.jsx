0036dfabbf61382d22bb5d9b08becc6afcb43d74
Alex Carufel <Carual42@gmail.com>
2023-01-14 14:04:36 -0600

    change reference

--- src/components/VisitCard/VisitCardOne.jsx
+++ src/components/VisitCard/VisitCardOne.jsx
@@ -72,7 +72,7 @@ function VisitCardOne() {
     dispatch({type: 'SET_MED_NOTES', payload: item.med_notes})
     dispatch({type: 'SET_PROCEDURE', payload: item.procedure_name})
     dispatch({type: 'SET_PROCEDURE_PHONE', payload: item.procedure_phone})
-    dispatch({type: 'SET_PROCEDURE_NOTES', payload: item.procedure_notes})
+    dispatch({type: 'SET_PROCEDURE_NOTES_for_doc', payload: item.procedure_notes})
     dispatch({type: 'SET_SCAN', payload: item.scan_name})
     dispatch({type: 'SET_SCAN_PHONE', payload: item.scan_phone})
     dispatch({type: 'SET_SCAN_NOTES_for_money', payload: item.scan_notes})
